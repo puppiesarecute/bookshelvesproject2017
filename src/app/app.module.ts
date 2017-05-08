f47127fdfcaf6d2a2104016a59a80f5035e1f7a8
Lê Trung Hiền Thục <thuc.le@matcao.vn>
2017-05-08 15:51:02 +0700

    add component folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MyFirstComponentComponent } from './my-first-component/my-first-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyFirstComponentComponent
   ],
   imports: [
     BrowserModule,
